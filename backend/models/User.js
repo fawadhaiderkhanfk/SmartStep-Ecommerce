8589420a74bcc10d92646bdb5126ba076fd56de4
Fawad Haider Khan <fawadhaiderkhanfk@gmail.com>
2026-03-14 02:06:14 +0500

    Fixed Google Login bug: Corrected async pre-save middleware in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs"); 
+const bcrypt = require("bcryptjs");
 
-// @desc    User schema definition
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -13,34 +12,31 @@ const userSchema = new mongoose.Schema({
         unique: true
     },
     password: {
-        type: String // Optional to accommodate Google OAuth users
+        type: String 
     },
     googleId: {
-        type: String // Stores Google ID for OAuth users
+        type: String 
     },
     role: {
         type: String,
         enum: ["user", "admin"],
-        default: "user" 
+        default: "user"
     }
 }, { timestamps: true });
 
-// @desc    Compare entered password with the hashed password in database
 userSchema.methods.matchPassword = async function (enteredPassword) {
     if (!this.password) return false;
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// @desc    Encrypt password before saving to the database
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        next();
+// @desc    Modern Async Pre-save Hook (Bina 'next' ke)
+userSchema.pre("save", async function () {
+    if (!this.isModified("password") || !this.password) {
+        return; 
     }
-    
-    if (this.password) {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", userSchema);
